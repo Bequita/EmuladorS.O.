3c0ab0475796399eb6c9816f241deac29a72ee80
Fernando <fernando@fernando-Satellite-L845.(none)>
2015-12-03 20:31:55 -0300

    ReadyQTest

--- Source/Code/ReadyQueue.py
+++ Source/Code/ReadyQueue.py
@@ -4,18 +4,6 @@ Created on Sep 15, 2015
 @author: fernando
 '''
 
-'''
-class ReadyQueue:
-    def __init__(self):
-        self.pcbs = Queue()
-
-    def addPcb(self, pcb):
-        self.pcbs.put(pcb)
-
-    def getPcb(self):
-        return self.pcbs.get
-'''
-
 class CeldaEnvejecimiento:
     def __init__(self):
         self.nivel1 = []
